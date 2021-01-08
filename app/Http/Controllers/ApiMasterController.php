f922625b130c39c87f13a3ccae0cb05e76afc893
cashtree <ogan@cashtree.id>
2021-01-08 17:51:29 +0700

    get province

--- app/Http/Controllers/ApiMasterController.php
+++ app/Http/Controllers/ApiMasterController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\CtreeCache;
+use App\Province;
 use Illuminate\Http\Request;
 
 class ApiMasterController extends ApiController
 {
     public function get_province(Request $request){
-        $province = CtreeCache::get_province(false);
+//        $province = CtreeCache::get_province(false);
+        $province = Province::where('row_status','=','active')->select('id','province_name')->get();
         $reponse =[
             'province'=>$province
         ];
