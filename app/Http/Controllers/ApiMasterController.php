0728cfcb026dcf82fba943aea63e3ee328ae4545
cashtree <ogan@cashtree.id>
2021-02-03 17:36:16 +0700

    get location

--- app/Http/Controllers/ApiMasterController.php
+++ app/Http/Controllers/ApiMasterController.php
@@ -29,7 +29,7 @@ class ApiMasterController extends ApiController
 
     public function get_all_locate(Request $request){
 
-        $get_all_location = Province::select('province.province_name','city.city_name')->join('city','city.province_id','=','province.id')->get();
+        $get_all_location = Province::get();
         $reponse =[
             'get_location'=>$get_all_location
         ];
