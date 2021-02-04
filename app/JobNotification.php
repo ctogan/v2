3b131739b342b52b6754baf365a54655dd42bfb5
cashtree <ogan@cashtree.id>
2021-02-04 15:19:59 +0700

    get notification

--- app/JobNotification.php
+++ app/JobNotification.php
@@ -8,10 +8,14 @@ class JobNotification extends Model
 {
     protected $connection = 'common';
     protected $table = 'job_notification';
-    protected $dateFormat = 'Y-m-d H:i:s';
     protected $fillable = [
         'uid','title','message','is_read', 'type','created_at','created_by','updated_at','updated_by'
     ];
+
+    protected function serializeDate(DateTimeInterface $date) : string
+    {
+        return $date->format('Y-m-d');
+    }
 
 
 }
