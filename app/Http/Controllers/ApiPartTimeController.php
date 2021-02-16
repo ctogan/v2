60ccd3b16b1c1e86ae2e5b948014b7a8aed6d994
cashtree <ogan@cashtree.id>
2021-02-16 17:00:28 +0700

    save filter category

--- app/Http/Controllers/ApiPartTimeController.php
+++ app/Http/Controllers/ApiPartTimeController.php
@@ -859,7 +859,7 @@ class ApiPartTimeController extends ApiController
     public function insert_preference_education(Request $request){
 
         $education = $request->id_education;
-        $educations = explode('.',$education);
+        $educations = explode(',',$education);
         foreach ($educations as $val){
 
             $data_insert = array(
@@ -876,7 +876,7 @@ class ApiPartTimeController extends ApiController
     public function insert_preference_category(Request $request){
 
         $category = $request->id_category;
-        $categories = explode('.',$category);
+        $categories = explode(',',$category);
         foreach ($categories as $val){
 
             $data_insert = array(
