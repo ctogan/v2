a2e7c1529a60e76a370ebaf7296fd48265abcfad
Ridcat Simbolon <ridcat@cashtree.id>
2021-02-22 11:34:02 +0700

    api city

--- app/Http/Controllers/ApiPartTimeController.php
+++ app/Http/Controllers/ApiPartTimeController.php
@@ -55,6 +55,7 @@ class ApiPartTimeController extends ApiController
         if($job_filter){
             $json = json_decode($job_filter->filter);
             print_r(explode(',' ,$json->province_id));
+            print_r(Province::select('province_name')->whereIn('id' , explode(',' ,$json->province_id))->get());
             exit;
             if(count($json > 0)){
                 $job_filter['province_id'] = Province::select('province_name')->whereIn('id' , explode(',' ,$json->province_id));
