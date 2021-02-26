bb4b05b90c3d4ff09c6a83e82d43a17697f9a482
Ridcat Simbolon <ridcat@cashtree.id>
2021-02-26 18:51:08 +0700

    api city

--- app/Http/Controllers/ApiPartTimeController.php
+++ app/Http/Controllers/ApiPartTimeController.php
@@ -466,8 +466,8 @@ class ApiPartTimeController extends ApiController
                 $user->email =$request->email;
                 $user->weight =$request->weight;
                 $user->height =$request->height;
-                $user->religion = $request->religion;
-                //$user->last_education = $request->education;
+                //$user->religion = $request->religion;
+                $user->last_education = $request->education;
                 $user->skills = $request->skill;
                 $user->hobby = $request->hobby;
                 $user->img = $request->img;
