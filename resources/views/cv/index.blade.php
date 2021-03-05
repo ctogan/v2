988529bda3a97f010544fc208f9b23b14b2968bd
ctagust <agust@cashtree.id>
2021-03-05 18:31:48 +0700

    cv

--- resources/views/cv/index.blade.php
+++ resources/views/cv/index.blade.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{--<link rel="preconnect" href="https://fonts.gstatic.com">--}}
     {{--<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet">--}}
@@ -164,7 +163,7 @@
             padding-bottom: 30px;
         }
     </style>
-    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('/assets/css/cv.css')}}" />
+    <link rel="stylesheet" type="text/css" media="all" href="/var/www/v2/public/css/cv.css" />
 </head>
 <body>
     <div class="cv-container">
