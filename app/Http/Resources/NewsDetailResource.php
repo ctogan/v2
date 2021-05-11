b13adc023de8dc47106c103ba4d5378bb2f62f16
ctagust <agust@cashtree.id>
2021-05-11 16:59:51 +0700

    news

--- app/Http/Resources/NewsDetailResource.php
+++ app/Http/Resources/NewsDetailResource.php
@@ -19,7 +19,8 @@ class NewsDetailResource extends JsonResource
             'title' => $this->title,
             'author' => $this->author,
             'url_to_image' => $this->url_to_image,
-            'content' => htmlspecialchars_decode(nl2br($this->content)),
+//            'content' => htmlspecialchars_decode(nl2br($this->content)),
+            'content' => $this->content,
             'view_count' => $this->view_count,
             'reward' => $this->reward
         ];
