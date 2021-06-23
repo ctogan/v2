36ef86592bb48724e7e036d436cc26aef3604bb4
ctagust <agust@cashtree.id>
2021-06-23 16:18:47 +0700

    prize redeem

--- app/PulsaBuy.php
+++ app/PulsaBuy.php
@@ -9,6 +9,8 @@ class PulsaBuy extends Model
     protected $connection = 'common';
     protected $table = 'pulsa_buy';
 
+    public $timestamps = false;
+
     protected $fillable = [
         'seq','uid','dt','pulsa_good_id','is_auto','tm','run_at','complete','changed','cash','status','req_count','phone','res_code','tr_id','additional_1','flash_detail_code'
     ];
