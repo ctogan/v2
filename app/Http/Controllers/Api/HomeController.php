3578f68d043109c8f49f211542f8830a22b41c68
capogan <ogan@capioteknologi.co.id>
2021-07-15 12:03:52 +0700

    event  buy

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -94,6 +94,7 @@ class HomeController extends ApiController
                 ->where('tm_end', '>=', date('Y-m-d H:i'))
                 ->get();;
         }
+        
 
         $query_flash_event = FlashEvent::where('row_status','=','active')->with('detail');
         if($user->is_tester){
@@ -104,36 +105,37 @@ class HomeController extends ApiController
         $arr_flash_event = $query_flash_event->get();
         $arr_flash = [];
         foreach ($arr_flash_event as $item){
-//            if($item->ut_by_register_date){
-//                $register = Carbon::parse(date_format(date_create($user->register),"Y-m-d"));
-//                $from = Carbon::parse(date_format(date_create($item->registered_from),"Y-m-d"));
-//                $to = Carbon::parse(date_format(date_create($item->registered_to),"Y-m-d"));
-//                if(!($register->gte($from) && $register->lte($to))){
-//                   continue;
-//                }
-//            }elseif ($item->ut_by_point_count){
-//                $point = $user->total_earn - $user->total_use;
-//                if(!($point >= $item->target_point_from && $point <= $item->target_point_to)){
-//                    continue;
-//                }
-//            }
-//
-//            if($item->event_period == 'weekly'){
-//                $date = date('Y-m-d');
-//                $d = new \DateTime($date);
-//                $day_name = $d->format('l');
-//                if($day_name == $item->day_name){
-//                    if(!($today->gte($item->event_start) && $today->lte($item->event_end))){
-//                        continue;
-//                    }
-//                }else{
-//                    continue;
-//                }
-//            }else{
-//                if(!($today->gte($item->event_start) && $today->lte($item->event_end))){
-//                    continue;
-//                }
-//            }
+
+            if($item->ut_by_register_date){
+                $register = Carbon::parse(date_format(date_create($user->register),"Y-m-d"));
+                $from = Carbon::parse(date_format(date_create($item->registered_from),"Y-m-d"));
+                $to = Carbon::parse(date_format(date_create($item->registered_to),"Y-m-d"));
+                if(!($register->gte($from) && $register->lte($to))){
+                   continue;
+                }
+            }elseif ($item->ut_by_point_count){
+                $point = $user->total_earn - $user->total_use;
+                if(!($point >= $item->target_point_from && $point <= $item->target_point_to)){
+                    continue;
+                }
+            }
+
+            if($item->event_period == 'weekly'){
+                $date = date('Y-m-d');
+                $d = new \DateTime($date);
+                $day_name = $d->format('l');
+                if($day_name == $item->day_name){
+                    if(!($today->gte($item->event_start) && $today->lte($item->event_end))){
+                        continue;
+                    }
+                }else{
+                    continue;
+                }
+            }else{
+                if(!($today->gte($item->event_start) && $today->lte($item->event_end))){
+                    continue;
+                }
+            }
 
             array_push($arr_flash, $item);
         }
