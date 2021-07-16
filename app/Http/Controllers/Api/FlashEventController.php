37c1ca9fc4f95ca9754e11cdca4bdc6cf7c3e2c8
capogan <ogan@capioteknologi.co.id>
2021-07-16 20:14:41 +0700

    flash sale

--- app/Http/Controllers/Api/FlashEventController.php
+++ app/Http/Controllers/Api/FlashEventController.php
@@ -248,16 +248,9 @@ class FlashEventController extends ApiController
                 return $this->errorResponse(static::ERROR_FLASH_EVENT_OUT_OF_STOCK,static::ERROR_CODE_FLASH_EVENT_OUT_OF_STOCK);
             }
 
+            $pulsa_goods = PulsaGoods::where('opcode','=',$user->opcode)->where('good_code','=',$flash_detail->product->product_code)->where('server_pulsa','=','MOBILEPULSA')->first();
 
-//
-//            $exist_goods  = PulsaBuy::where('pulsa_goods_id','=',$pulsa_goods->id)
-//                ->where('uid','=',$user->uid)
-//                ->where('dt', date('Y-m-d'))->count();
-//
-//
-//            if($exist_goods > 0){
-//                return $this->errorResponse(static::ERROR_FLASH_BUY_DUPLICATE,static::ERROR_CODE_FLASH_BUY_DUPLICATE);
-//            }
+
 
             $trans = PulsaBuy::create(
                 ([
